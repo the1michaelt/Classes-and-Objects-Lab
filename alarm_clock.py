4d077eb6756dc6a0af9377ef112cfd8c037236d5
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-08-24 16:14:53 -0400

    Draft of alarm clock lab

--- alarm_clock.py
+++ alarm_clock.py
@@ -2,14 +2,17 @@ class AlarmClock:
     def __init__(self): #constructor; instance variables below; what is has.
         self.time="" # current time = ""
         self.alarm_status= ["on", "off"] # whether the alarm is on or off =['on','off']
-        self.alarm_time= "# time the alarm is set to = ""
+        self.alarm_time= "" # time the alarm is set to
         
-    def current_time(self): #methods; what it can do.
-        time= '12:00'
-        print(f'The current time is ', {time})
-        return time
+# inquiry_one = AlarmClock("12:00")
+# inquiry_one.current_time()  # display time
+    def current_time(self, current_time):  # methods; what it can do.
+        print(f'The current time is ', {current_time})
+        return current_time
         
-    def set_or_change_current_time(self, time):
+# inquiry_two = AlarmClock()
+# inquiry_two.time  # change time
+    def set_or_change_current_time(self, current_time):
         want_change= input('Would you like to change the time? y/n')
         if want_change=='y':
             change_time = input('What time would you like to set? ')
@@ -17,29 +20,36 @@ class AlarmClock:
             return time
         else:
             print(f'Your time remains ', {time})
-    
+
+# inquiry_three = AlarmClock()
+# inquiry_three.alarm_status  # display alarm status
     def turn_alarm_on_off(self, alarm_status):
         alarm_status= 'Off'
+# inquiry_four = AlarmClock()
+# inquiry_four.alarm_time  # change alarm status
         want_change = input('Would you like to turn on alarm? y/n')
         if want_change == 'y':
-            alarm_status = 'On'
-            Print ('Your alarm is now ',{alarm_status})
-            return alarm_status
+            change_alarm_status = 'On'
+            Print ('Your alarm is now ',{change_alarm_status})
+            return change_alarm_status
         else:
-            print(f'Your alarm remains ', {alarm_status})
+            print(f'Your alarm remains ', {change_alarm_status})
         
-            
+# inquiry_five = AlarmClock()
+# inquiry_five.alarm_time  # display alarm time
     def time_of_alarm(self, alarm_time):
-        alarm_time= '15:00'
+        # alarm_time= '15:00'
         print("Your alarm is set to ", {alarm_time})
         return alarm_time
-    
+
+# inquiry_six = AlarmClock()
+# inquiry_six.alarm_time  # change alarm
     def set_or_change_alarm_time(self, alarm_time):
-        want_change = input('Would you like to turn on alarm? y/n')
+        want_change = input('Would you like to change your alarm? y/n')
         if want_change == 'y':
             change_alarm = input('What time would you like to be your alarm? ')
-            Print('Your alarm is now ', {alarm_time})
-            return alarm_time
+            Print('Your alarm is now ', {change_alarm})
+            return change_alarm
     
 
 # class variables to keep track of the AlarmClock’s 
--- main.py
+++ main.py
@@ -1,3 +1,46 @@
 from alarm_clock import AlarmClock
 
+inquiry_one= AlarmClock("12:00")
+inquiry_one.current_time() #display time
 
+inquiry_two= AlarmClock("Y")
+inquiry_two.change_time  # change time
+
+inquiry_three= AlarmClock("off")
+inquiry_three.alarm_status  # display alarm status
+
+inquiry_four= AlarmClock("N")
+inquiry_four.change_alarm_status  # change alarm status
+
+inquiry_five= AlarmClock("15:00")
+inquiry_five.alarm_time #display alarm time
+
+inquiry_six= AlarmClock("Y")
+inquiry_six.change_alarm  # change alarm
+
+
+# Three objects
+       self.time = "" # current time = ""
+       # whether the alarm is on or off =['on','off']
+       self.alarm_status = ["on", "off"]
+       self.alarm_time =
+
+# class variables to keep track of the AlarmClock’s
+# current time,
+# whether the alarm is on or off,
+# time the alarm is set to.
+# NOTE: You can use arbitrary strings to represent the time, it does not need to be accurate or change over time
+
+# method to set (or change) the current time and
+# print to the console the current time. return ___
+
+# toggle the alarm on or off. 
+
+# a method to set the current alarm time  input?
+# and print to the console the current alarm time.  print or return ()
+
+# import the AlarmClock class into main.py so I can instantiate it as a new AlarmClock object and call methods on it.
+
+# Print the clock’s time to the terminal print(___)
+
+# Call the alarm clock’s change time method to change the time

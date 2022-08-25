a8bbfc67a10ab6ee5dc85787d471ae4288ad9c00
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-08-24 21:44:14 -0400

    Draft revision of alarm clock

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,57 +1,72 @@
 class AlarmClock:
-    def __init__(self, time): #constructor; instance variables below; what is has.
-        self.time= time # current time = ""
-        self.alarm_status= ["on", "off"] # whether the alarm is on or off =['on','off']
-        self.alarm_time= "" # time the alarm is set to
-        
-# inquiry_one = AlarmClock("12:00")
-# inquiry_one.time()  # display time
+    # constructor; instance variables below; what is has.
+    def __init__(self, time, time_change_YN, alarm_status, alarm_time, alarm_change_YN):
+        self.time = time  # default time # one_display_time = AlarmClock
+        self.time_change_YN = time_change_YN  # two_change_time = AlarmClock
+        # three_alarm_status = AlarmClock # whether the alarm is on or off =['on','off']
+        self.alarm_status = alarm_status # alarm time set # four_alarm_time = AlarmClock
+        self.alarm_time = alarm_time  # four_alarm_time.time_of_alarm()  #
+        self.alarm_change_YN = alarm_change_YN  # five_change_alarm.set_or_change_alarm_time()  #
+
+
+# one_display_time = AlarmClock("12:00", "", "", "", "")
+# one_display_time.current_time()  # function to_display_time
+
+# two_change_time = AlarmClock("", "Y", "", "", "")
+# two_change_time.set_or_change_current_time()  # change_time
+
+# three_alarm_status = AlarmClock("", "", "off", "", "")
+# three_alarm_status.turn_alarm_on_off()  # display_alarm_status
+
+# four_alarm_time = AlarmClock("", "", "", "15:00", "")
+# four_alarm_time.time_of_alarm()  # display_alarm_time
+
+# five_change_alarm = AlarmClock("", "", "", "", "Y")
+# five_change_alarm.set_or_change_alarm_time()  # change_alarm
+
+
+# inquiry_one = AlarmClock("12:00") # inquiry_one.time()  # display time
     def current_time(self):  # methods; what it can do.
-        print(f'The current time is ', self.time)
+        print('The default time is ', self.time)
         
-        
-# inquiry_two = AlarmClock()
-# inquiry_two.time  # change time
+# inquiry_two = AlarmClock() # inquiry_two.time  # change time
     def set_or_change_current_time(self):
-        want_change= input('Would you like to change the time? y/n ')
-        if want_change=='y':
-            time= '12:00'
-            change_time = input('What time would you like to set? ')
-            print(f'Your time is now ', change_time)
-            # return time
-        else:
-            print(f'Your time remains ', time)
-
-# inquiry_three = AlarmClock()
-# inquiry_three.alarm_status  # display alarm status
-    def turn_alarm_on_off(self, alarm_status):
-        # alarm_status= 'Off'
-# inquiry_four = AlarmClock()
-# inquiry_four.alarm_time  # change alarm status
-        want_change = input('Would you like to turn on alarm? y/n')
+        want_change= input('Would you like to change the default time from 12:00? y/n ')
         if want_change == 'y':
-            change_alarm_status = 'On'
-            Print ('Your alarm is now ',change_alarm_status)
-            # return change_alarm_status
+            change_time = input('What time would you like to set? Enter one between 0:00-23:59.')
+            print('Your time is now ',change_time)
         else:
-            print(f'Your alarm remains ', change_alarm_status)
-        
-# inquiry_five = AlarmClock()
-# inquiry_five.alarm_time  # display alarm time
-    def time_of_alarm(self, alarm_time):
-        # alarm_time= '15:00'
-        print("Your alarm is set to ", alarm_time)
-        # return alarm_time
+            print('Your time remains 12:00.')
 
-# inquiry_six = AlarmClock()
-# inquiry_six.alarm_time  # change alarm
-    def set_or_change_alarm_time(self, alarm_time):
-        want_change = input('Would you like to change your alarm? y/n')
+# inquiry_three = AlarmClock() # inquiry_three.alarm_status  # display alarm status
+    def turn_alarm_on_off(self):
+        want_change = input('Would you like to turn on an alarm? y/n ')
+        if want_change != 'y':
+            print('Your alarm is now Off.')
+        else:
+            print('Your alarm remains On.')
+            time_of_alarm()
+        
+# inquiry_four = AlarmClock() # inquiry_four.alarm_time  # display alarm time
+    def time_of_alarm(self):
+        print("Your default alarm is set to 15:00")
+        alarm_status = input('Would you like to change the default time? y/n ')
+        if alarm_status == 'y':
+            change_time = input('What time would you like to set? Enter one between 0:00-23:59.')
+            print('Your alarm is now ', change_time)
+        else:
+            print('Your alarm remains at 15:00')
+
+# inquiry_five = AlarmClock() # inquiry_five.alarm_time  # change alarm
+    def set_or_change_alarm_time(self):
+        want_change = input('Would you like to change your alarm? y/n ')
         if want_change == 'y':
             change_alarm = input('What time would you like to be your alarm? ')
             Print('Your alarm is now ', change_alarm)
-            # return change_alarm
-    
+
+
+            
+
 
 # # class variables to keep track of the AlarmClock’s 
 # # current time, 
@@ -72,4 +87,21 @@ class AlarmClock:
 # Print the clock’s time to the terminal print(___)
 
 # Call the alarm clock’s change time method to change the time
+
+# ("12:00") == time
+# one_display_time== object  
+# # .current_time() # function to display time
+
+# ("Y") == change_time
+# two_change_time.set_or_change_current_time()  # 
+
+# ("off") == display_alarm_status
+# three_alarm_status.turn_alarm_on_off()  # 
+
+# ("15:00")== display_alarm_time
+# four_alarm_time.time_of_alarm()  # 
+
+# ("Y") ==  new_alarm_time
+# five_change_alarm.set_or_change_alarm_time()  # 
+
 
--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 from alarm_clock import AlarmClock
 
-
 # inquiry_one= AlarmClock()
-inquiry_one = AlarmClock("12:00")
-inquiry_one.current_time()  # function to display time
+one_display_time = AlarmClock("12:00","","","","")
+one_display_time.current_time()  # function to display time
 
-inquiry_two= AlarmClock("Y")
-inquiry_two.set_or_change_current_time()  # change time
+two_change_time= AlarmClock("","Y","","","")
+two_change_time.set_or_change_current_time()  # change time
 
-inquiry_three= AlarmClock("off")
-inquiry_three.turn_alarm_on_off(alarm_status)  # display alarm status
+three_alarm_status= AlarmClock("","","off","","")
+three_alarm_status.turn_alarm_on_off()  # display alarm status
 
-# inquiry_four= AlarmClock("N")
-# inquiry_four.change_alarm_status  # change alarm status
+four_alarm_time= AlarmClock("","","","15:00","")
+four_alarm_time.time_of_alarm() #display alarm time
 
-inquiry_five= AlarmClock("15:00")
-inquiry_five.time_of_alarm(alarm_time) #display alarm time
-
-inquiry_six= AlarmClock("Y")
-inquiry_six.set_or_change_alarm_time(alarm_time)  # change alarm
+five_change_alarm = AlarmClock("","","","","Y")
+five_change_alarm.set_or_change_alarm_time()  # change alarm
 
 
 # # # Three objects
@@ -30,8 +26,8 @@ inquiry_six.set_or_change_alarm_time(alarm_time)  # change alarm
 # # # class variables to keep track of the AlarmClock’s
 # # # current time,
 # # # whether the alarm is on or off,
-# # # time the alarm is set to.
-# # # NOTE: You can use arbitrary strings to represent the time, it does not need to be accurate or change over time
+# time the alarm is set to.
+# NOTE: You can use arbitrary strings to represent the time, it does not need to be accurate or change over time
 
 # # # method to set (or change) the current time and
 # # # print to the console the current time. return ___
